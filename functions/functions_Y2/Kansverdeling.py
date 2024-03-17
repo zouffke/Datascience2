86d675b994c2f22f86a0b881b98d51a8b6f4d65b
Luca Dandois <luca.dandois@student.kdg.be>
2024-03-17 21:09:32 +0100

    poisson

--- functions/functions_Y2/Kansverdeling.py
+++ functions/functions_Y2/Kansverdeling.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import math
 
 
 def kansverdeling(data, xlabel: str, ylabel: str, title: str = "Kansverdeling"):
@@ -8,3 +9,21 @@ def kansverdeling(data, xlabel: str, ylabel: str, title: str = "Kansverdeling"):
     ax.bar(data[0], data[1])
     ax.set_title(title)
     plt.show()
+
+
+def average(value, probability, distribution: str = "default"):
+    if distribution == "default":
+        return (value * probability).sum()
+    if distribution == "binomial":
+        return value * probability
+    else:
+        raise Exception("Invalid distribution. Please use 'default' or 'binomial'.")
+
+
+def standard_deviation(value, probability, distribution: str = "default"):
+    if distribution == "default":
+        return math.sqrt(((value - average(value, probability)) ** 2 * probability).sum())
+    if distribution == "binomial":
+        return math.sqrt(value * probability * (1 - probability))
+    else:
+        raise Exception("Invalid distribution. Please use 'default' or 'binomial'.")
--- functions/functions_Y2/StandardDeviationAndAverage.py
+++
@@ -1,9 +0,0 @@
-import math
-
-
-def average(value, probability):
-    return (value * probability).sum()
-
-
-def standard_deviation(value, probability):
-    return math.sqrt(((value - average(value, probability)) ** 2 * probability).sum())
