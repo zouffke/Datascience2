4cbea2f8c7e3ad72aa4791a381ea4f2e7b8248b4
Luca Dandois <luca.dandois@student.kdg.be>
2024-03-28 14:37:47 +0100

    addedlift function

--- functions/functions_Y2/Ar.py
+++ functions/functions_Y2/Ar.py
@@ -1,6 +1,16 @@
-
 def confidence(itemsets, a, b):
     """
     Compute the confidence of the rule a -> b.
     """
-    return itemsets['support'].loc[itemsets['itemsets'] == {a, b}].values[0] / itemsets['support'].loc[itemsets['itemsets'] == {a}].values[0]
+    return __get_support_value__(itemsets, {a, b}) / __get_support_value__(itemsets, {a})
+
+
+def lift(itemsets, a, b):
+    """
+    Compute the lift of the rule a -> b.
+    """
+    return confidence(itemsets, a, b) / __get_support_value__(itemsets, {b})
+
+
+def __get_support_value__(itemsets, item):
+    return itemsets['support'].loc[itemsets['itemsets'] == item].values[0]
