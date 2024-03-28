5163c6fe85e6a497b8df6023493ed79accd71b07
Luca Dandois <luca.dandois@student.kdg.be>
2024-03-28 15:16:04 +0100

    finished AR

--- functions/functions_Y2/Ar.py
+++ functions/functions_Y2/Ar.py
@@ -19,6 +19,12 @@ def leverage(itemsets, a, b):
     return __get_support_value__(itemsets, {a, b}) - __get_support_value__(itemsets, {a}) * __get_support_value__(
         itemsets, {b})
 
+def conviction(itemsets, a, b):
+    """
+    Compute the conviction of the rule a -> b.
+    """
+    return (1 - __get_support_value__(itemsets, {b})) / (1 - confidence(itemsets, a, b))
+
 
 def __get_support_value__(itemsets, item):
     return itemsets['support'].loc[itemsets['itemsets'] == item].values[0]
