8350ed340072394d7d5666488eefa6222b764537
Luca Dandois <luca.dandois@student.kdg.be>
2024-03-28 17:18:44 +0100

    finished studentverdeling

--- functions/functions_Y2/Testing.py
+++ functions/functions_Y2/Testing.py
@@ -1,15 +1,23 @@
 import math
 from scipy.stats import norm
+from scipy import stats
 
 
-def betrouwbaarheids_interval(data, b):
+def betrouwbaarheids_interval(data, b: float = 0.95, t: bool = True):
     """
     :param data: The data that is used to calculate the confidence interval
     :param b: the confidence level
+    :param t: wetter to use the t-distribution or the normal distribution
     :return: the lower and upper bound of the confidence interval
     """
+    if not 0 < b < 1:
+        raise ValueError("b must be between 0 and 1")
+
     n = len(data)
-    f = norm.ppf((1 + b) / 2)
+    if t:
+        f = stats.t.ppf((1 + b) / 2, n - 1)
+    else:
+        f = norm.ppf((1 + b) / 2)
     M = data.mean()
     s = data.std()
     l = M - f * s / math.sqrt(n)
