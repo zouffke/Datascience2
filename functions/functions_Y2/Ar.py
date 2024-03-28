93978623a33eaa207427ccc7a932585a0cfc827b
Luca Dandois <luca.dandois@student.kdg.be>
2024-03-28 15:01:37 +0100

    added leverage function

--- functions/functions_Y2/Ar.py
+++ functions/functions_Y2/Ar.py
@@ -12,5 +12,13 @@ def lift(itemsets, a, b):
     return confidence(itemsets, a, b) / __get_support_value__(itemsets, {b})
 
 
+def leverage(itemsets, a, b):
+    """
+    Compute the leverage of the rule a -> b.
+    """
+    return __get_support_value__(itemsets, {a, b}) - __get_support_value__(itemsets, {a}) * __get_support_value__(
+        itemsets, {b})
+
+
 def __get_support_value__(itemsets, item):
     return itemsets['support'].loc[itemsets['itemsets'] == item].values[0]
